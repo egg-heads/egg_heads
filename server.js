340af0d58589183c1ad3ce7eaf301d332263f4d7
Morgan Fogarty <mofo37@gmail.com>
2017-05-08 11:08:18 -0700

    make server

---
+++ server.js
@@ -0,0 +1,12 @@
+/* eslint no-console: "off"*/
+const app = require('./lib/app');
+const http = require('http');
+require('./lib/connect');
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('server running on', server.address());
+});
