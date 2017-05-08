a0c3252246036dbca9b9a4b75341d9f5a9e3915f
Morgan Fogarty <mofo37@gmail.com>
2017-05-08 11:13:48 -0700

    add port to console.log

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 /* eslint no-console: "off"*/
 const app = require('./lib/app');
 const http = require('http');
-require('./lib/connect');
 
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log('server running on', server.address());
+  console.log('server running on', server.address().port);
 });
