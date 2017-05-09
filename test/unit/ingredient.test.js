8a297bc2dd086a08bf590968d4a4a06ee6fc21f5
Morgan Fogarty <mofo37@gmail.com>
2017-05-09 15:04:32 -0700

    ingredient unit test passing.

--- test/unit/ingredient.test.js
+++ test/unit/ingredient.test.js
@@ -5,10 +5,7 @@ describe('ingredient validation', () => {
 
   it.only('validates an ingredient', () => {
     const testIngredient = new Ingredient({
-      
-      ingredient: {
-        name: 'carrot',
-      }
+      name: 'carrot',
     });
 
     return testIngredient.validate();
