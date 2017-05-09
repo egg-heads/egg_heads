7773ed642d1834ef71b6721b642ba24e904de801
Morgan Fogarty <mofo37@gmail.com>
2017-05-09 10:54:43 -0700

    e2e auth test failing

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -1,33 +1,34 @@
-// const db = require('./db');
-// const request = require('./request');
-// const assert = require('chai').assert;
+const db = require('./db');
+const request = require('./request');
+const assert = require('chai').assert;
+const user = require('../../lib/models/user');
 
-// describe('auth tests', () => {
-//   // let token = '';
+describe('auth tests', () => {
+  let token = '';
 
-//   before(db.drop);
+  before(db.drop);
 
-//   // const user = {
-//   //   email: 'coolemail@me.com',
-//   //   password: 'yipee'
-//   // };
+  const user = {
+    email: 'mofo37@me.com',
+    password: 'catsrkewl'
+  };
 
-//   describe('user management', () => {
+  describe('user management', () => {
 
-//     const badRequest = (url, data, code, error) => {
-//       return request
-//         .post(url)
-//         .send(data)
-//         .then(
-//         () => { throw new Error('status should be not ok'); },
-//         res => {
-//           assert.equal(res.status, code);
-//           assert.equal(res.response.body.error, error);
-//         });
-//     };
+    const badRequest = (url, data, code, error) => {
+      return request
+        .post(url)
+        .send(data)
+        .then(
+        () => { throw new Error('status should not be okay'); },
+        res => {
+          assert.equal(res.status, code);
+          assert.equal(res.response.body.error, error);
+        });
+    };
 
-//     it('signup requires email', () => {
-//       return badRequest('/auth/signup', { password: 'yipee' }, 400, 'email and password required');
-//     });
-//   });
-// });
+    it('signup requires email', () => {
+      return badRequest('/auth/signup', { password: 'catsrkewl' }, 400, 'email and password required');
+    });
+  });
+});
