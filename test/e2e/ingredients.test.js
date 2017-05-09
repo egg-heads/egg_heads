fb57f32c7d598af02b596310621165fb8830b121
nicky evers <livelyevers@gmail.com>
2017-05-09 15:57:56 -0700

    added ingredient schema and beginning of route

---
+++ test/e2e/ingredients.test.js
@@ -0,0 +1,27 @@
+const assert = require('chai').assert;
+const db = require('./db');
+const request = require('./request');
+
+describe('ingredients API', () => {
+
+  before(db.drop);
+
+  let token = '';
+
+  const user = {
+    email: 'sup@suuuup.com',
+    password: 'yippeeeee'
+  };
+
+  before(() => {
+    return request.post('/auth/signup')
+      .send(user)
+      .then(res => token = res.body.token);
+  });
+
+  it('initial GET returns empty array', () => {
+    return request.get('/ingredients')
+      .set('Authorization', token)
+      .then(res => assert.deepEqual(res.body, []));
+  });
+});
