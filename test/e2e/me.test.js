249d1807d87d1ced1b53da3beb6061c6a0a2798d
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-09 10:44:05 -0700

    GET /me test failing

---
+++ test/e2e/me.test.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+const db = require('./db');
+const request = require('./request');
+
+describe('/me API', () => {
+
+  before(db.drop);
+
+  let token = '';
+
+  const user = {
+    email: 'sup@suuuup.com',
+    password: 'yippeeeee'
+  };
+
+  before(() => {
+    return request.post('/auth/signup')
+      .send(user)
+      .then(res => token = res.body.token);
+  });
+
+  it('initial GET returns empty array', () => {
+    return request.get('/me')
+      .set('Authorization', token)
+      .then(res => assert.deepEqual(res.body, []));
+  });
+
+});
