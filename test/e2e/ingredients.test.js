f5abcc965e28d2cb3555434e24593f081902528c
nicky evers <livelyevers@gmail.com>
2017-05-09 17:33:25 -0700

    ingredients GET ALL test passing

--- test/e2e/ingredients.test.js
+++ test/e2e/ingredients.test.js
@@ -28,6 +28,9 @@ describe.only('ingredients API', () => {
   let testIngredient = {
     name: 'cilantro'
   };
+  let testIngredient2 = {
+    name: 'beef'
+  };
 
   it('saves an ingredient', () => {
     return request.post('/ingredients')
@@ -37,6 +40,27 @@ describe.only('ingredients API', () => {
       .then(saved => {
         assert.ok(saved._id);
         testIngredient = saved;
+      });
+  });
+
+  it('saves another ingredient', () => {
+    return request.post('/ingredients')
+      .set('Authorization', token)
+      .send(testIngredient2)
+      .then(res => res.body)
+      .then(saved => {
+        assert.ok(saved._id);
+        testIngredient2 = saved;
+      });
+  });
+
+  it('gets all ingredients', () => {
+    return request.get('/ingredients')
+      .set('Authorization', token)
+      .then(res => res.body)
+      .then(ingredients => {
+        assert.equal(ingredients.length, 2);
+        assert.equal(ingredients[0].name, 'cilantro');
       });
   });
 });
