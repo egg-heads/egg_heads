85926ef7e8fd0191645560caa15e60398d3a2825
Morgan Fogarty <mofo37@gmail.com>
2017-05-10 14:28:04 -0700

    fixed 'post and get to fridge' route.

--- test/e2e/me.test.js
+++ test/e2e/me.test.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const db = require('./db');
 const request = require('./request');
 
-describe.only('/me API', () => {
+describe('/me API', () => {
 
   before(db.drop);
 
@@ -14,15 +14,7 @@ describe.only('/me API', () => {
   };
 
   let testIngredients = [
-    {
-      name: 'tea'
-    },
-    {
-      name: 'ice cream'
-    },
-    {
-      name: 'shishkabob'
-    }
+    { name: 'tea' }, { name: 'ice cream' }, { name: 'shishkabob' }
   ];
 
   before(() => {
@@ -37,39 +29,52 @@ describe.only('/me API', () => {
       .then(res => assert.equal(res.body.email, user.email));
   });
 
-  it('saves ingredients', () => {
-    return request.post('/ingredients')
-      .set('Authorization', token)
-      .send(testIngredients)
-      .then(res => res.body)
-      .then(savedIngredients => {
-        assert.ok(savedIngredients[0]._id);
-        testIngredients = savedIngredients;
-      });
+  describe('fridge post', () => {
+
+    before(() => {
+      return request.post('/ingredients')
+        .set('Authorization', token)
+        .send(testIngredients)
+        .then(res => res.body)
+        .then(savedIngredients => {
+          assert.ok(savedIngredients[0]._id);
+          testIngredients = savedIngredients;
+        });
+    });
+
+    it('adding single ingredient to fridge', () => {
+      let fridgeItem = { ingredient: testIngredients[0]._id };
+
+      return request.post('/me/fridge')
+        .set('Authorization', token)
+        .send(fridgeItem)
+        .then(res => res.body)
+        .then(fridgeArray => {
+          assert.equal(fridgeArray[0].ingredient, testIngredients[0]._id);
+        });
+    });
+
+    it('adding multiple ingredients to fridge', () => {
+      let fridgeItem = [{ ingredient: testIngredients[0]._id, expiration: new Date() }, { ingredient: testIngredients[1]._id, expiration: new Date() }];
+
+      return request.post('/me/fridge')
+        .set('Authorization', token)
+        .send(fridgeItem)
+        .then(res => res.body)
+        .then(fridgeArray => {
+          assert.equal(fridgeArray[0].ingredient, testIngredients[0]._id);
+          assert.equal(fridgeArray.length, 3);
+        });
+    });
+
+    it('gets ingredients in fridge', () => {
+      return request.get('/me/fridge')
+        .set('Authorization', token)
+        .then(res => res.body)
+        .then(fridgeIngredients => {
+          assert.equal(fridgeIngredients.length, 3);
+        });
+
+    });
   });
-
-  it('adding ingredients to fridge', () => {
-    let fridgeItem = [testIngredients[0]._id, testIngredients[1]._id];
-
-    return request.post('/me/fridge')
-      .set('Authorization', token)
-      .send(fridgeItem)
-      .then(res => res.body)
-      .then(fridgeArray => {
-        assert.equal(fridgeArray, fridgeItem);
-      });
-  });
-
-  // TODO: find out why we can't add an array of ingredients into the fridge field, only one item or else we get a single empty object'
-
-  it('gets ingredients in fridge', () => {
-    return request.get('/me/fridge')
-      .set('Authorization', token)
-      .then(res => res.body)
-      .then(fridgeIngredients => {
-        assert.equal(fridgeIngredients.length, 1);
-      });
-
-  });
-
 });
